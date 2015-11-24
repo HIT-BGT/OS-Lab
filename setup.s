52919d4588211665ea76526559a9d225cb1e586a
黄渝光 <yugungwong@gmail.com>
2015-11-24 15:40:07 +0800

    use stack. update doc

--- setup.s
+++ setup.s
@@ -1,22 +1,6 @@
 !
 !	setup.s		(C) 1991 Linus Torvalds
 !
-! setup.s is responsible for getting the system data from the BIOS,
-! and putting them into the appropriate places in system memory.
-! both setup.s and system has been loaded by the bootblock.
-!
-! This code asks the bios for memory/disk/other parameters, and
-! puts them in a "safe" place: 0x90000-0x901FF, ie where the
-! boot-block used to be. It is then up to the protected mode
-! system to read them from there before the area is overwritten
-! for buffer-blocks.
-!
-
-! NOTE! These had better be the same as in bootsect.s!
-
-INITSEG  = 0x9000	! we move boot here - out of the way
-SYSSEG   = 0x1000	! system loaded at 0x10000 (65536).
-SETUPSEG = 0x9020	! this is the current segment
 
 .globl begtext, begdata, begbss, endtext, enddata, endbss
 .text
@@ -27,13 +11,21 @@ begdata:
 begbss:
 .text
 
-entry start
-start:
+BOOTSEG  = 0x07c0			! original address of boot-sector
+INITSEG  = 0x9000			! we move boot here - out of the way
+SETUPSEG = 0x9020			! setup starts here
 
-go:	mov	ax,cs
-	mov	ds,ax
-	mov	es,ax
+! ROOT_DEV:	0x000 - same type of floppy as boot.
+!		0x301 - first partition on first drive etc
+ROOT_DEV = 0x306
+
+entry _start
+_start:
+
 ! Print some inane message
+	mov	ax,#SETUPSEG
+	mov ds,ax
+	mov	es,ax
 
 	mov	ah,#0x03		! read cursor pos
 	xor	bh,bh
@@ -41,214 +33,165 @@ go:	mov	ax,cs
 	
 	mov	cx,#25
 	mov	bx,#0x0007		! page 0, attribute 7 (normal)
-	mov	bp,#msg2
+	mov	bp,#msg1
 	mov	ax,#0x1301		! write string, move cursor
 	int	0x10
 
-! ok, the read went well so we get current cursor position and save it for
-! posterity.
-
+!fetch the cursor location
 	mov	ax,#INITSEG	! this is done in bootsect already, but...
 	mov	ds,ax
 	mov	ah,#0x03	! read cursor pos
 	xor	bh,bh
 	int	0x10		! save it in known place, con_init fetches
 	mov	[0],dx		! it from 0x90000.
-! Get memory size (extended mem, kB)
 
+!fetch memory size
 	mov	ah,#0x88
 	int	0x15
 	mov	[2],ax
 
-! Get video-card data:
-
-	mov	ah,#0x0f
-	int	0x10
-	mov	[4],bx		! bh = display page
-	mov	[6],ax		! al = video mode, ah = window width
-
-! check for EGA/VGA and some config parameters
-
-	mov	ah,#0x12
-	mov	bl,#0x10
-	int	0x10
-	mov	[8],ax
-	mov	[10],bx
-	mov	[12],cx
-
-! Get hd0 data
-
+!fetch hard drive info
 	mov	ax,#0x0000
 	mov	ds,ax
 	lds	si,[4*0x41]
 	mov	ax,#INITSEG
 	mov	es,ax
-	mov	di,#0x0080
+	mov	di,#0x0004
 	mov	cx,#0x10
 	rep
 	movsb
+    call print_nl
 
-! Get hd1 data
+! Print "Cursor POS:"
+	push #11
+	push #cursor
+	call print_string
+	pop ax
+	pop ax
+	
+! Print cusor pos
+	mov bp, #0x00
+	call print_hex
+	call print_nl
 
-	mov	ax,#0x0000
-	mov	ds,ax
-	lds	si,[4*0x46]
-	mov	ax,#INITSEG
+! Print "Memory SIZE"
+	push #12
+	push #memory
+	call print_string
+	pop	ax
+	pop ax
+	
+!Print memory size
+	mov bp, #0x02
+	call print_hex
+!Print KB
+	push #2
+	push #KB
+	call print_string
+	pop ax
+	pop	ax
+	call print_nl
+
+! Print "Cyls:"
+	push #5
+	push #cyls
+	call print_string
+	pop ax
+	pop ax
+! Print cyls:
+	mov bp, #0x04
+	call print_hex
+	call print_nl
+
+! Print "Heads:"
+	push #6
+	push #heads
+	call print_string
+	pop ax
+	pop ax
+! Print heads:
+	mov bp, #0x06
+	call print_hex
+	call print_nl
+
+! Print "Sectors:"
+	push #8
+	push #_sectors
+	call print_string
+	pop ax
+	pop ax
+! Print sectors:
+	mov bp, #0x12
+	call print_hex
+	call print_nl
+
+inf_loop:
+	jmp inf_loop
+
+print_string:
+	mov	ax,#SETUPSEG
 	mov	es,ax
-	mov	di,#0x0090
-	mov	cx,#0x10
-	rep
-	movsb
+	mov	ah,#0x03		! read cursor pos
+	xor	bh,bh
+	int	0x10
+	add esp, #4
+	mov cx, (esp)
+	mov	bx,#0x0007
+	sub esp, #2
+	mov bp, (esp)
+	sub esp, #2		! ret PC
+	mov ax,#0x1301
+	int 0x10
+	ret
 
-	!mov ax, #INITSEG
-	!mov ds, ax
-	!mov ax, #SETUPSEG
-	!mov es, ax
-
-print_cur:
-  mov ah,#0x03
-  xor bh,bh
-  int 0x10        
-  mov ax,#SETUPSEG
-  mov es,ax
-  mov cx,#11
-  mov bx,#0x0007
-  mov bp,#msg3
-  mov ax,#0x1301
-  int 0x10
-  xor di,di
-  mov ax,#INITSEG
-  mov ds,ax
-  mov bx,(di)
-  call print_bx
-  call print_nl
-
-print_mem:
-  mov ah,#0x03
-  xor bh,bh
-  int 0x10        
-  mov cx,#12
-  mov bx,#0x0007
-  mov bp,#msg4
-  mov ax,#0x1301
-  int 0x10
-  
-  mov di,#0x02
-  mov bx,(di)
-  call print_bx
-
-  mov ah,#0x03
-  xor bh,bh
-  int 0x10 
-  mov cx,#2
-  mov bx,#0x0007
-  mov bp,#msg5
-  mov ax,#0x1301
-  int 0x10
-  call print_nl
-
-print_hd:
-  mov ah,#0x03
-  xor bh,bh
-  int 0x10        
-  mov cx,#5
-  mov bx,#0x0007
-  mov bp,#msg6
-  mov ax,#0x1301
-  int 0x10 
-  mov di,#0x80
-  mov bx,(di)
-  call print_bx
-
-  mov ah,#0x03
-  xor bh,bh
-  int 0x10 
-  mov cx,#1
-  mov bx,#0x0007
-  mov bp,#msg7
-  mov ax,#0x1301
-  int 0x10    
-  call print_nl
-
-
-  mov ah,#0x03
-  xor bh,bh
-  int 0x10        
-  mov cx,#6
-  mov bx,#0x0007
-  mov bp,#msg8
-  mov ax,#0x1301
-  int 0x10 
-  mov di,#0x82
-  mov bx,(di)
-  xor bh,bh
-  call print_bx
-  call print_nl
-  
-  mov ah,#0x03
-  xor bh,bh
-  int 0x10        
-  mov cx,#8
-  mov bx,#0x0007
-  mov bp,#msg9
-  mov ax,#0x1301
-  int 0x10 
-  mov di,#0x8e
-  mov bx,(di)
-  xor bh,bh
-  call print_bx
-  call print_nl
-
-death_loop:
-	jmp death_loop
-
-print_bx:
-   mov cx,#4
-   mov dx,bx
+print_hex:
+    mov	cx,#4 		! 4 hex numbers
+    mov	dx,(bp) 	! move the number dp points to to dx
 print_digit:
-   rol dx,#4
-   mov ax,#0x0e0f
-   and al,dl
-   add al,#0x30
-   cmp al,#0x3a
-   jb outp
-   add al,#0x07
-outp:
-   int 0x10
-   loop print_digit
-   ret
+    rol	dx,#4		
+    mov	ax,#0xe0f 	! ah = 0x0E, al = ASCII code of what is going to show
+    and	al,dl
+    add	al,#0x30 
+    cmp	al,#0x3a
+    jl	outp  		!if it is smaller than 10
+    add	al,#0x07  	!plus 7 more
+outp: 
+    int	0x10
+    loop	print_digit
+    ret
 
 print_nl:
-   mov ax,#0x0e0d
-   int 0x10
-   mov al,#0x0a
-   int 0x10
-   ret
+    mov	ax,#0xe0d 	! CR
+    int	0x10
+    mov	al,#0xa 	! LF
+    int	0x10
+    ret
 
-msg2:
+sectors:
+	.word 0
+
+msg1:
 	.byte 13,10
 	.ascii "Now we are in SETUP"
-	.byte 13,10,13,10	
-
-msg3:
-    .ascii "Cursor POS:"
-	 
-msg4:
-    .ascii "Memory SIZE:"
-	 
-msg5:
+	.byte 13,10,13,10
+cursor:
+	.ascii "Cursor POS:"
+memory:
+	.ascii "Memory SIZE:"
+KB:
 	.ascii "KB"
+cyls:
+	.ascii "Cyls:"
+heads:
+	.ascii "Heads:"
+_sectors:
+	.ascii "Sectors:"
 
-msg6:
-    .ascii "Cyls:"	 
-
-msg7:
-	.ascii "A"
-
-msg8:
-    .ascii "Heads:"
-msg9:
-    .ascii "Sectors:"
+.org 508
+root_dev:
+	.word ROOT_DEV
+boot_flag:
+	.word 0xAA55
 
 .text
 endtext:
